534f1a7fe84c119c678a938adbdbd515f6835b88
MacUser <omar.garcia.developer@hotmail.com>
2017-09-07 13:17:57 -0500

    Agregado de estilos dinamicos

---
+++ EjemploEstilos/Views/DynamicInhetitancePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EjemploEstilos.Views
+{
+    public partial class DynamicInhetitancePage : ContentPage
+    {
+		bool originalStyle = true;
+
+		public DynamicInhetitancePage()
+		{
+			InitializeComponent();
+			Resources["searchBarStyle"] = Resources["blueSearchBar"];
+		}
+
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			if (originalStyle)
+			{
+				Resources["searchBarStyle"] = Resources["greenSearchBar"];
+				originalStyle = false;
+			}
+			else
+			{
+				Resources["searchBarStyle"] = Resources["blueSearchBar"];
+				originalStyle = true;
+			}
+		}
+    }
+}
---
+++ EjemploEstilos/Views/DynamicStylePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EjemploEstilos.Views
+{
+    public partial class DynamicStylePage : ContentPage
+    {
+        bool originalStyle = true;
+
+        public DynamicStylePage()
+        {
+            InitializeComponent();
+            Resources["searchBarStyle"] = Resources["blueSearchStyle"];
+        }
+
+        void OnButtonClicked(object sender, EventArgs e) {
+            if(originalStyle) {
+                Resources["searchBarStyle"] = Resources["greenSearchStyle"];
+                originalStyle = false;
+            } else {
+				Resources["searchBarStyle"] = Resources["blueSearchStyle"];
+				originalStyle = true;
+            }
+        }
+    }
+}
