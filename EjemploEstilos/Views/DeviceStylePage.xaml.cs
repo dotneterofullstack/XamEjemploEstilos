3e5b96f20aef2cc018b4a2d3dda16dae429fdeee
MacUser <omar.garcia.developer@hotmail.com>
2017-09-07 16:24:30 -0500

    * EjemploEstilos.csproj: Agregado de estilos de dispositivos
    * EjemploEstilosPage.xaml:
    * DeviceStylePage.xaml:
    * DeviceStylePage.xaml.cs:

---
+++ EjemploEstilos/Views/DeviceStylePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EjemploEstilos.Views
+{
+    public partial class DeviceStylePage : ContentPage
+    {
+        public DeviceStylePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
